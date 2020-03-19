acd6002607022932baf82a3db25eb4f0e7fd3d30
Shixinyu <hit1190300908@163.com>
2020-03-20 00:04:08 +0800

    map2 commit.

--- app/src/main/java/com/bingzhu/Map.java
+++ app/src/main/java/com/bingzhu/Map.java
@@ -1,10 +1,13 @@
 package com.bingzhu;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class Map extends AppCompatActivity {
     Button button1;
@@ -14,15 +17,28 @@ public class Map extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map);
 
-        //设置联系人的按钮活动--------------------------
-        button2 = findViewById(R.id.send_button);
+        //设置联系人的按钮活动-------------------------------------
+        button2 = findViewById(R.id.settingcontact_button);
         button2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
-
+                showListDialog();
             }
         });
 
     }
-}
+
+    //设置联系人的对话框方法--------------------------------------------
+    private void showListDialog(){
+        final String[] items = {"联系人1", "联系人2"};
+        AlertDialog.Builder listDialog = new AlertDialog.Builder(Map.this);
+        listDialog.setTitle("选取您要设置的紧急联系人");
+        listDialog.setItems(items, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                Toast.makeText(Map.this,"点击了"+items[which],Toast.LENGTH_SHORT).show();
+            }
+        });
+        listDialog.show();
+    }
+ }
